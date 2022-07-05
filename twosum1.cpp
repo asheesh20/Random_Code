2adf00c72d404f0864c1f3219dfacc6ce57efc95
Asheesh <98988175+asheesh20@users.noreply.github.com>
2022-07-06 05:09:46 +0530

    1. Two Sum

---
+++ twosum1.cpp
@@ -0,0 +1,36 @@
+// !st Approach 
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> ans;
+        for(int i=0;i<nums.size()-1;i++){
+            for(int j=i+1;j<nums.size();j++){
+                if(nums[i]+nums[j]==target){
+                    return {i,j};
+                }
+            }
+        }
+        return {0};
+    }
+};
+
+
+// 2nd Approach 
+
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> ans;
+        for(int i=0;i<nums.size()-1;i++){
+            for(int j=i+1;j<nums.size();j++){
+                if(nums[i]+nums[j]==target){
+                    ans.push_back(i);
+                    ans.push_back(j);
+                }
+            }
+        }
+        return ans;
+    }
+};
