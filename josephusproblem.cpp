3d71dabc2810ca6ab015ec223f47549c25b1cb53
Asheesh Kumar <asheesh220720@gmail.com>
2022-08-08 14:36:48 +0530

    Josephus problem

---
+++ josephusproblem.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+    public:
+    int jos(int n,int k){
+        if(n==1) return 0;
+        return (jos(n-1,k)+k)%n;
+    }
+    int josephus(int n, int k)
+    {
+       return jos(n,k)+1;
+    }
+};
+int main() {	
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int n,k;
+		cin>>n>>k;
+		Solution ob;
+		cout<<ob.josephus(n,k)<<endl;
+	}
+	return 0;
+}
