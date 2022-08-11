0fa9f7cea935c10445674b8e9e60cec8a22e9488
Asheesh Kumar <asheesh220720@gmail.com>
2022-08-11 14:41:24 +0530

    Print Like A Wave

---
+++ printlikewave.cpp
@@ -0,0 +1,17 @@
+vector<int> wavePrint(vector<vector<int>> arr, int nRows, int mCols)
+{
+    vector<int> v;
+    for(int i=0;i<mCols;i++){
+        if(i&1) {
+            for(int j=nRows-1;j>=0;j--){
+                v.push_back(arr[j][i]);
+            }
+        }
+        else {
+            for(int j=0;j<nRows;j++){
+                v.push_back(arr[j][i]);
+            }
+        }
+    }
+    return v;
+}
