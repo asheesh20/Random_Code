ff313a2ccb86902f46fff45d9b15257275bd8f13
Asheesh Kumar <asheesh220720@gmail.com>
2022-08-11 14:53:49 +0530

    Wave Array

---
+++ wavearray.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution{
+    public:
+    void convertToWave(int n, vector<int>& arr){
+    for(int i=0;i<n-1;i+=2){
+        if(i+1<n){
+            swap(arr[i],arr[i+1]);
+        }
+    }
+    }
+};
+int main()
+{
+    int t,n;
+    cin>>t; 
+    while(t--) 
+    {
+        cin>>n; 
+        vector<int> a(n); 
+        for(int i=0;i<n;i++)
+            cin>>a[i]; 
+        Solution ob;
+        ob.convertToWave(n, a);
+        for(int i=0;i<n;i++)
+            cout<<a[i]<<" "; 
+            
+        cout<<endl;
+    }
+}
